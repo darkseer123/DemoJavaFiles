0aaaffb58e0dd9dd3054057894e684dbff2f1bc9
Ashu Gupta <darklord.ashu@gmail.com>
2011-12-26 00:45:00 +0530

    a

---
+++ src/com/ashu/javasamples/BigSum.java
@@ -0,0 +1,18 @@
+/**
+ * Program to find sum of digits of a very large number like 2^1000
+ */
+
+package com.ashu.javasamples;
+
+public class BigSum {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+	    
+
+	}
+
+}
