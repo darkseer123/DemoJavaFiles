cadb8dd9808c0dfb77b6853a249f028dec16cbe2
Ashu Gupta <darklord.ashu@gmail.com>
2011-12-26 03:51:43 +0530

    a

--- src/com/ashu/sinkdotcom/SimpleDotCom.java
+++ src/com/ashu/sinkdotcom/SimpleDotCom.java
@@ -1,33 +1,39 @@
 package com.ashu.sinkdotcom;
 
+import java.util.ArrayList;
+
 class SimpleDotCom {
 
-	private int[] locationCells;
-	private int numOfHits;
-
-	void setLocationCells(int[] loc) {
+	private String dotcomName;
+	private ArrayList<String> locationCells;
+	
+	void setDotComName (String name) {
+		dotcomName = name; 
+	}
+	
+	void setLocationCells(ArrayList<String> loc) {
 		locationCells = loc;
+	}
+	
+	String getDotComName () {
+		return dotcomName;
 	}
 
 	public String checkYourself(String stringGuess) {
 
-		int guess = Integer.parseInt(stringGuess);
 		String result = "miss";
 
-		for (int cell : locationCells) {
-			if (guess == cell) {
+		int index = locationCells.indexOf(stringGuess);
+		if (index >= 0) {
+			locationCells.remove(index);
+			if (locationCells.isEmpty()) {
+				result = "kill";
+			} else {
 				result = "hit";
-				numOfHits++;
-				break;
 			}
 		}
-
-		if (numOfHits == locationCells.length) {
-			result = "kill";
-		}
-
+		
 		System.out.println(result);
-
 		return result;
 	}
 }
--- src/com/ashu/sinkdotcom/SimpleDotComTestDrive.java
+++ src/com/ashu/sinkdotcom/SimpleDotComTestDrive.java
@@ -1,25 +1,38 @@
 package com.ashu.sinkdotcom;
 
+import java.util.ArrayList;
+
 public class SimpleDotComTestDrive {
 
 	public static void main(String[] args) {
-		SimpleDotCom simple = new SimpleDotCom();
 		GameHelper helper = new GameHelper();
-		int[] loc = { 2, 3, 4 };
+		SimpleDotCom simple = new SimpleDotCom();
+		
+		String url = "http://www.goibibo.com";
+		
+		ArrayList<String> loc = new ArrayList<String>();
+		loc.add("2");
+		loc.add("3");
+		loc.add("4");
+				
 		String result = "failed";
-
 		String userGuess = "";
+		
+		int numOfGuesses = 0;
 
 		boolean isAlive = true;
 
+		simple.setDotComName(url);
 		simple.setLocationCells(loc);
 
 		while (isAlive) {
 			if (result == "kill") {
 				isAlive = false;
+				System.out.printf("You took %d guesses to kill %s\n", numOfGuesses, simple.getDotComName());
 			} else {
 				userGuess = helper.getUserInput("Enter a number: ");
 				result = simple.checkYourself(userGuess);
+				numOfGuesses++;
 
 			}
 
