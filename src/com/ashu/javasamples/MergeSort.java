6cafe9405dcf51fe1d2ccea2fc946fa16585b23e
ashu <darklord.ashu@gmail.com>
2011-12-26 18:04:49 +0530

    MergeSort Functional Example

--- src/com/ashu/javasamples/MergeSort.java
+++ src/com/ashu/javasamples/MergeSort.java
@@ -4,12 +4,13 @@ public class MergeSort {
 
 	public static void main(String[] args) {
 
-		int[] arr = { 2, 5, 7, 6, 3, 1, 4, 8 };
-		Merge merge = new Merge();
+		int[] arr = { 8, 5, 7, 1, 6, 3, 4, 2, 10, 9 };
+		Merge merge = new Merge(arr);
 		merge.setArray(arr);
 		merge.sort(0, arr.length - 1);
 		int[] result = merge.getArray();
 
+		System.out.println("The sorted array is: ");
 		for (int i = 0; i < result.length; i++) {
 			System.out.println(result[i]);
 		}
@@ -21,22 +22,26 @@ class Merge {
 
 	private int[] array;
 	private int[] result;
-	private int resultIndex = 0;
+	private int resultIndex;
+
+	Merge(int[] a) {
+		array = a;
+		result = new int[a.length];
+		resultIndex = 0;
+	}
 
 	public void setArray(int[] a) {
 		array = a;
 	}
 
 	public int[] getArray() {
-		return result;
+		return array;
 	}
 
 	public void sort(int start, int end) {
 
-		int mid = (start + end) / 2;
-		
-		System.out.println(start + "   " + mid + "  "+ end);
-		if (start <= end) {
+		if (start < end) {
+			int mid = (start + end) / 2;
 			sort(start, mid);
 			sort(mid + 1, end);
 			merge(start, mid, end);
@@ -44,20 +49,20 @@ class Merge {
 	}
 
 	private void merge(int start, int mid, int end) {
+
 		int s1 = start;
 		int s2 = mid + 1;
 
+		resultIndex = 0;
+
 		while (s1 <= mid && s2 <= end) {
 			if (array[s1] < array[s2]) {
-				result[resultIndex++] = array[s1];
-				s1++;
+				result[resultIndex++] = array[s1++];
 			} else if (array[s1] > array[s2]) {
-				result[resultIndex++] = array[s2];
-				s2++;
+				result[resultIndex++] = array[s2++];
 			} else {
-				result[resultIndex++] = array[s1];
-				s1++;
-				s2++;
+				result[resultIndex++] = array[s1++];
+				result[resultIndex++] = array[s2++];
 			}
 		}
 
@@ -69,11 +74,9 @@ class Merge {
 			result[resultIndex++] = array[s2++];
 		}
 
-		for (int i : result) {
-			System.out.println("Result now: ");
-			System.out.println(i);
+		for (int i = start, j = 0; i <= end; i++, j++) {
+			array[i] = result[j];
 		}
-
 	}
 
 }
